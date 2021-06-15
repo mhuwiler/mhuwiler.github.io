c8dbce4697ff670fb35cb7a98fc8d72d1ddf9643
Marc Huwiler <marc.huwiler@cern.ch>
2021-06-15 23:47:13 +0200

    Folder structure with index.md for each page

--- source/buildWebsite.py
+++ source/buildWebsite.py
@@ -39,6 +39,9 @@ def copyFolderContent(sourcedir, destdir, navfile, indent, instance):
 			copyFolderContent(source, destination, navfile, indent+1, instance)
 		elif (os.path.isfile(src)): 
 			if (".md" in item): # only copy .md files
+				filedest = dest.replace(".md", "")
+				os.system("mkdir -p "+filedest)
+				dest = filedest+"/index.md"
 				if (debug): print "\tcopying file: {} to: {}".format(src, dest)
 				destinationfile = open(dest, "w")
 				with open(src, "r") as infile:
